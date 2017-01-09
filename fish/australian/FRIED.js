df03cdef734b98e551ee9f189740f07f6cac8a5c
Paul Scherer <paul.scherer@mrc-epid.cam.ac.uk>
2017-01-09 10:04:34 +0000

    Australian cohort

--- fish/australian/FRIED.js
+++ fish/australian/FRIED.js
@@ -1,1 +1,4 @@
-Fried fish including take away ( fishfri_00)   
+// Fried fish including take away ( fishfri_00)   
+
+friedFish = $('fishfri_00').value();
+friedFish;
--- fish/australian/TOTAL.js
+++ fish/australian/TOTAL.js
@@ -1,1 +1,11 @@
-Sum all variables:  Fish - steamed, grilled or baked  (fishgri_00)  + Fried fish including take away (fishfri_00)   + Fish - tinned (salmon, tuna, sardines etc.) (fishtin_00)
+// Sum all variables:  
+// Fish - steamed, grilled or baked  (fishgri_00)  
+// + Fried fish including take away (fishfri_00)   
+// + Fish - tinned (salmon, tuna, sardines etc.) (fishtin_00)
+
+fishgri_00 = $('fishgri_00').value();
+fishfri_00 = $('fishfri_00').value();
+fishtin_00 = $('fishtin_00').value();
+
+total = fishgri_00 + fishfri_00 + fishtin_00;
+total;
